587df01ec18f4eef52bbdc084e39e31c9b9824df
AKITO <dipeshkoli86@gmail.com>
2025-03-24 14:52:47 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,10 +1,10 @@
 # IF YOU CAN'T USE ENV VARIABLES FILL EVERYTHING HERE
 
-VAR_SESSION = "BQCDh4MAHV8uvnYaJBGhIGQi2iXZFAm13XFMimWBuHR1LBkCuTYLyKuXeSLlHr4WCK8gMbtScxgKn39-1cFpqUUbtieEyC8DE_lFH0i63oYqi4xUOCCBWi6FaQKF2G23QwA-sTvbV0J3_-KYv8K8MA2k0urvApv7wHShyyzCLESoKc3GgDWUhVrjqDPrbJ0hPOLe3CFbMN2kjfgafkAQOAPyunmpr36NrFQTtsQ7QuMKSnZxiqRj0SnTbu2r_cSfdAFaY4A_Z59h4P3q1t5hzA9BR_hgItKA1HWAGrCnasmAwoHoUfHyvvEuuGKEMJDGefSGByxJmDBDQDKTM-IAiJR-HGjQSgAAAAGNiQ4WAA"
+VAR_SESSION = "BQCDh4MAky_kJr57OktGA63aG-ODDup1N5ierx-9w-FTnBQjzoNP-MT_vJh3lIki3qhaPbPXTIh9n9p8NMwdtHN6CjrJMbzgIxhaaYtTaqarwN18-l8tpPCx-UbV3IVk6H1OtUJpc77naFp1zk57VSseaYe3ZD31efbif1C6WU7NvyTQy_wFUaSbDhUmW-QZDbWhZcJg9d51oFF2bTh5FBIgDAtG0_r1nA9u3NFCqcnnp_C7ztHI_0KTqGrZOwKowOQcE-o-y_mfku7AbFJWubztynt6m_u8A-BUcaDBne2TyzzcLx-4apCfU1TyhwiwhauatYNLLzkt9q-dsVQgeE33duXmYgAAAAGLVDWlAA"
 VAR_API_ID = "10187126"
 VAR_API_HASH = "ff197c0d23d7fe54c89b44ed092c1752"
 VAR_MONGO_DB_URI = "mongodb+srv://publicDB:publicDBbyKira@public.twckcqf.mongodb.net/?retryWrites=true&w=majority"
-VAR_TOKEN = "7682841659:AAFmr2L-pLoo16oDCTScX9gSz3P0no5Qi1E"
-VAR_REPO_URL = "https://github.com/THE-AKITO/UB/" # Change to your fork repo
+VAR_TOKEN = "7682841659:AAFmr2L-7476823979:AAFDgyR3QaZmGYyos3jv6sa4U1gh31papaM"
+VAR_REPO_URL = "https://github.com/THE-AKITO/UBx/" # Change to your fork repo
 
 # Use "" for strings
